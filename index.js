86a70c7c4a13988cc710a8e0fac74bf3e2da8266
girOly <girardob@gmail.com>
2019-06-28 20:10:13 +0000

    Recursive function

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  for (num in array) {
+    if (Array.isArray(array[num]) == true) {
+      sum += sumItems(array[num]);
+    } else {
+      sum += array[num];
+    }
+  }
+  console.log(array[num] + ", " + sum + "\n");
+  return sum;
 }
 
 module.exports = sumItems;
+
+sumItems([25]);
+
+// function printItems(array) {
+//   array.forEach((item) => {
+//     if (Array.isArray(item)) {
+//       // Print out all it's items individually
+//       printItems(item);
+//     } else {
+//       console.log(item);
+//     }
+//   });
+// }
